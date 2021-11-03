5505fd85c15cd7423f6c0f7de8c08a678690fd57
christianperea <cperea1995@gmail.com>
2021-11-03 01:38:18 -0400

    Initial commit

---
+++ src/test/java/com/spring/saggezza/technicalclass/FlowControlTest.java
@@ -0,0 +1,99 @@
+package com.spring.saggezza.technicalclass;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class FlowControlTest {
+
+    @InjectMocks
+    private FlowController flowController;
+
+    @Mock
+    private IIntTester intTesterMock;
+
+    @Mock
+    private IBoundaryChecker boundaryCheckerMock;
+
+    @Test
+    public void givenOneCallIsIntegerOnce() {
+
+        // Given: I am a user
+
+        String input = "1";
+
+
+        //given(intTesterMock.isInteger("1")).willReturn(true);
+
+        // When: I enter the string “1”
+        flowController.run(input);
+
+        // Then: call isInteger() once
+
+        then(intTesterMock).should(times(1)).isInteger(input);
+
+    }
+
+    @Test
+    public void givenZeroCallIsIntegerOnce() {
+
+        // Given: I am a user
+
+        String input = "0";
+
+
+        // When: I enter the string “0”
+//        when(intTesterMock.isInteger("0")).thenReturn(true);
+        flowController.run(input);
+
+        // Then: call isInteger() once
+
+        then(intTesterMock).should(times(1)).isInteger(input);
+
+
+    }
+
+    @Test
+    public void givenBobCallIsIntegerOnce() {
+
+        // Given: I am a user
+        String input = "Bob";
+
+        // When: I enter the string “Bob”
+        flowController.run(input);
+
+//        when(intTesterMock.isInteger("Bob")).thenReturn(false);
+
+        // Then: call isInteger() once
+        then(intTesterMock).should(times(1)).isInteger(input);
+
+
+    }
+
+    @Test
+    public void givenOneCallIsInsideBoundaryOnce() {
+
+        // Given: I am a user
+        String input = "1";
+
+
+        // When: I enter the string “0”
+        flowController.run(input);
+
+//        when(intTesterMock.isInteger("0")).thenReturn(true);
+
+        // Then: call isInteger() once
+        then(intTesterMock).should(times(1)).isInteger(input);
+
+
+    }
+
+}
